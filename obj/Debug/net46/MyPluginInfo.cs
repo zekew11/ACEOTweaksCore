033696c1b02790d8ab9b81cd4d89c65abd1860ab
zekew11 <zekelukew@gmail.com>
2024-07-03 15:08:55 -0700

    init

---
+++ obj/Debug/net46/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace ACEOTweaksCore
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "ACEOTweaksCore";
+        public const string PLUGIN_NAME = "ACEOTweaksCore";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
